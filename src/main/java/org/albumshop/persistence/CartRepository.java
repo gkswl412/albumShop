a84767cabd36232a2a02381f5eeb651e42fab5c4
jungcomet <jungcomet9@gmail.com>
2021-12-09 10:09:08 +0900

    CartRepository 생성

---
+++ src/main/java/org/albumshop/persistence/CartRepository.java
@@ -0,0 +1,8 @@
+package org.albumshop.persistence;
+
+import org.albumshop.domain.Cart;
+import org.albumshop.domain.MultiIdUserAlbum;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CartRepository extends CrudRepository<Cart, MultiIdUserAlbum> {
+}
