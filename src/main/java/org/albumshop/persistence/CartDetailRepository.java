3e2e54995a7973826f9f4b581a26812d4efd4449
jungcomet <jungcomet9@gmail.com>
2021-12-09 17:43:24 +0900

    CartService -> 장바구니에 담기 구현중

--- src/main/java/org/albumshop/persistence/CartDetailRepository.java
+++ src/main/java/org/albumshop/persistence/CartDetailRepository.java
@@ -2,8 +2,10 @@ package org.albumshop.persistence;
 
 import org.albumshop.domain.CartDetail;
 import org.albumshop.domain.MultiIdCartAlbum;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CartDetailRepository extends CrudRepository<CartDetail, MultiIdCartAlbum> {
-    //CartDetail findByCartIdAndAlbumId(Long cartId, Long albumId);
+    @Query("select cd from CartDetail cd where cd.multiId.cart = ?1 and cd.multiId.album = ?2")
+    CartDetail findByCartIdAndAlbumId(Long cartId, Long albumId);
 }
--- src/main/java/org/albumshop/service/CartService.java
+++ src/main/java/org/albumshop/service/CartService.java
@@ -1,16 +1,50 @@
 package org.albumshop.service;
 
-import org.albumshop.domain.Album;
-import org.albumshop.domain.Cart;
-import org.albumshop.domain.MultiIdUserAlbum;
-import org.albumshop.domain.User;
+import org.albumshop.domain.*;
 import org.albumshop.persistence.AlbumRepository;
+import org.albumshop.persistence.CartDetailRepository;
+import org.albumshop.persistence.CartRepository;
+import org.albumshop.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 @Transactional
 public class CartService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CartRepository cartRepository;
+    @Autowired
+    CartDetailRepository cartDetailRepository;
+    @Autowired
+    AlbumRepository albumRepository;
 
+    public MultiIdCartAlbum addCart(CartDetail cartDetail, String userId) {
+        Album album = albumRepository.findById(cartDetail.getMultiId().getAlbum().getId())
+                .orElseThrow(EntityNotFoundException::new);
+        User user = userRepository.findById(userId).orElseThrow(EntityNotFoundException::new);
+
+        Cart cart = cartRepository.findByUserId(userId);
+
+        if (cart == null) {
+            cart = Cart.createCart(user);
+            cartRepository.save(cart);
+        }
+
+        CartDetail savedCartAlbum = cartDetailRepository.findByCartIdAndAlbumId(cart.getId(), album.getId());
+
+        if (savedCartAlbum != null) {
+            savedCartAlbum.addQuantity(cartDetail.getQuantity());
+            return savedCartAlbum.getMultiId();
+        } else {
+            CartDetail newCartDetail = CartDetail.createCartDetail(cart, album, cartDetail.getQuantity());
+            cartDetailRepository.save(newCartDetail);
+            return newCartDetail.getMultiId();
+        }
+
+    }
 }
