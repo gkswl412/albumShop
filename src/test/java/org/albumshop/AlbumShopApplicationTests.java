305f2940655bb95541f8d86433e683cfb32b3729
KwonOhTae <quite84@gmail.com>
2021-12-09 14:04:16 +0900

    albumshopapplicationtests update

--- src/test/java/org/albumshop/AlbumShopApplicationTests.java
+++ src/test/java/org/albumshop/AlbumShopApplicationTests.java
@@ -3,19 +3,32 @@ package org.albumshop;
 import java.time.LocalDate;
 
 import org.albumshop.domain.Album;
+import org.albumshop.domain.ArtistGroup;
 import org.albumshop.persistence.AlbumRepository;
+import org.albumshop.persistence.ArtistGroupRepository;
+import org.albumshop.persistence.ArtistRepository;
+import org.albumshop.persistence.ReviewRepository;
 import org.albumshop.persistence.SongRepository;
-import org.junit.jupiter.api.Test;
+import org.albumshop.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlbumShopApplicationTests {
 
+	
+	@Autowired
+	UserRepository uRepo;
 	@Autowired
 	AlbumRepository abRepo;
 	@Autowired
 	SongRepository sRepo;
+	@Autowired
+	ArtistGroupRepository agRepo;
+	@Autowired
+	ArtistRepository aRepo;
+	@Autowired
+	ReviewRepository rRepo;
 	
 	
 	// @Test
@@ -45,11 +58,21 @@ class AlbumShopApplicationTests {
 		}
 	}
 	
-	@Test
-	public void artistinsert() {
+	// @Test
+	public void ArtistGroupinsert() {
+		//ArtistGroup ag = ArtistGroup.builder().name("아티스트그룹A").debutDate("2000-10-2")
+		ArtistGroup ag1 = new ArtistGroup();
+		ag1.setName("아티스트그룹A");
+		ag1.setDebutDate(LocalDate.of(2000, 10, 1));
+		agRepo.save(ag1);
 		
-		
+		ArtistGroup ag2 = new ArtistGroup();
+		ag2.setName("아티스트그룹B");
+		ag2.setDebutDate(LocalDate.of(2001, 9, 11));
+		agRepo.save(ag2);
 	}
+	
+	
 	
 	
 }
