03c45a4d7f8bdbc2180c140f35cf230016e16874
junsiku <junsik7511@gmail.com>
2021-12-16 17:50:57 +0900

    회원가입 기능 추가

---
+++ src/main/java/org/albumshop/security/UserService.java
@@ -0,0 +1,63 @@
+package org.albumshop.security;
+
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
+
+import org.albumshop.domain.User;
+import org.albumshop.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class UserService implements UserDetailsService{
+	@Autowired
+	private HttpSession httpSession;
+	
+	@Autowired
+	private UserRepository urepo;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder; // security config에서 Bean생성
+	
+	
+
+
+	// 회원가입
+	@Transactional
+	public User joinUser(User user) {
+		// 비밀번호 암호화...암호화되지않으면 로그인되지않는다.
+		      String pass = passwordEncoder.encode(user.getPass());
+		      user.setPass(pass);
+		System.out.println("암호화된 pass:" + pass);
+		return urepo.save(user);
+	}
+
+	//반드시 구현해야한다. 
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String id) throws UsernameNotFoundException {
+		System.out.println("loadUserByUsername id:" + id);
+		//filter는 조건에 맞는것만 선택
+		//map: 변형한다. 
+	 
+		UserDetails user = urepo.findById(id)
+				.filter(m -> m != null).map(m -> new SecurityUser(m)).get();
+		System.out.println("user:" + user);
+		httpSession.setAttribute("user", user);
+		return user;
+	}
+
+	 
+	
+}
+ 
