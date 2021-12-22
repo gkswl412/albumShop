23d16fe188385521dd78a828770d7a5815decfb9
KwonOhTae <quite84@gmail.com>
2021-12-22 18:53:00 +0900

    review count

--- src/main/java/org/albumshop/persistence/ArtistGroupRepository.java
+++ src/main/java/org/albumshop/persistence/ArtistGroupRepository.java
@@ -3,9 +3,13 @@ package org.albumshop.persistence;
 import java.util.List;
 
 import org.albumshop.domain.ArtistGroup;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ArtistGroupRepository extends CrudRepository<ArtistGroup, Long>{
 
 	List<ArtistGroup> findByNameContaining(String name);
+	
+	@Query(value = "select count(id) from artist_group ", nativeQuery = true)
+	public Long countAllById();
 }
