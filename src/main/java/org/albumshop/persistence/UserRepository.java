c9b489882cf22da31ef4e56eccf5bad2b1405ec5
junsiku <junsik7511@gmail.com>
2021-12-23 15:08:19 +0900

    회원가입 기능

--- src/main/java/org/albumshop/persistence/UserRepository.java
+++ src/main/java/org/albumshop/persistence/UserRepository.java
@@ -1,10 +1,16 @@
 package org.albumshop.persistence;
 
+import java.util.Optional;
+
 import org.albumshop.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface UserRepository extends CrudRepository<User, String>{
 	
+	Optional<User> findByNickName(String nickName);
+	Optional<User> findByEmail(String email);
 
 	
 }
