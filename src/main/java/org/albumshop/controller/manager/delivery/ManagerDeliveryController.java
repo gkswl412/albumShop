4f7eb8665eee7ad8330cbe539bee7c5cfbbdab1a
jungcomet <jungcomet9@gmail.com>
2022-01-06 15:17:22 +0900

    주문 상세내역 페이지 구성중

--- src/main/java/org/albumshop/controller/manager/delivery/ManagerDeliveryController.java
+++ src/main/java/org/albumshop/controller/manager/delivery/ManagerDeliveryController.java
@@ -3,7 +3,6 @@ package org.albumshop.controller.manager.delivery;
 
 import lombok.extern.java.Log;
 import org.albumshop.domain.Delivery;
-import org.albumshop.service.manager.Delivery1Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,22 +13,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/manager/delivery/")
 @Log
 public class ManagerDeliveryController {
+/*
 
     @Autowired
-    Delivery1Service delivery1Service;
+    DeliveryService deliveryService;
 
     @GetMapping("/deliveryList")
     public void deliveryList(Model model){
+*/
 /*        List<Delivery> deliveryList = deliveryService.readAllDeliveries();
-        model.addAttribute("deliveryList", deliveryList);*/
+        model.addAttribute("deliveryList", deliveryList);*//*
+
         log.info("DeliveryList called...");
     }
 
     @GetMapping("/deliveryInfo")
     public void deliveryInfo(Long id, Model model){
-        Delivery delivery = delivery1Service.readDelivery(1l);
+        Delivery delivery = deliveryService.readDelivery(1l);
         model.addAttribute("delivery", delivery);
         log.info("DeliveryId called...");
     }
+*/
 
 }
