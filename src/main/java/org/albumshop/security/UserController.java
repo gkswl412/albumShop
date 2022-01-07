5b413d8cb66b38c15028dde8a0b36d6a3a865cc9
junsiku <junsik7511@gmail.com>
2022-01-07 12:41:01 +0900

    비밀번호 찾기 구현

--- src/main/java/org/albumshop/security/UserController.java
+++ src/main/java/org/albumshop/security/UserController.java
@@ -1,25 +1,23 @@
 package org.albumshop.security;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletResponse;
+
 import javax.servlet.http.HttpSession;
 
 import org.albumshop.domain.User;
 import org.albumshop.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+                      
+
 
 @Controller
 public class UserController {
